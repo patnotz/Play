276c760aeb5505b6da8ec2f206745c5a91a4c3c6
Pat Notz <patnotz@gmail.com>
2011-08-03 13:15:25 -0600

    proof of concept for runtime and compile time traits functions

--- src/Play.hpp
+++ src/Play.hpp
@@ -2,9 +2,49 @@
 #ifndef SRC_PLAY_HPP_
 #define SRC_PLAY_HPP_
 
+#include <iostream>
+
 namespace play {
 typedef double ScalarT;
 typedef int IndexT;
+
+// This "trait" class has the dimensions fixed at compile time
+template <int NDIM1, int NDIM2>
+struct TraitC {
+  static void set_dims(const int dim1, const int dim2) {
+    if (dim1 != NDIM1 || dim2 != NDIM2) {
+      throw "can't set_dims on compile time traits";
+    }
+  }
+  static int ndim1() { return NDIM1; }
+  static int ndim2() { return NDIM2; }
+};
+
+// This "trait" class allows you to change the dimensions
+// via static members at runtime
+struct TraitR {
+  static void set_dims(const int dim1, const int dim2) {
+    s_ndim1 = dim1;
+    s_ndim2 = dim2;
+  }
+  static int ndim1() { return s_ndim1; }
+  static int ndim2() { return s_ndim2; }
+private:
+  static int s_ndim1;
+  static int s_ndim2;
+};
+int TraitR::s_ndim1 = 0;
+int TraitR::s_ndim2 = 0;
+
+// A templated class that uses traits to get dimensions.
+// Question: if T is such that ndimX are known at compile
+// time will the optimizer unroll loops, etc.?
+template <class T>
+struct Expr {
+  int product() {
+    return T::ndim1() * T::ndim2();
+  }
+};
 
 }  // namespace play
 
--- src/unit-tests.cpp
+++ src/unit-tests.cpp
@@ -10,3 +10,27 @@ TEST(Play, Pass)
 {
   ASSERT_TRUE(SIZE > 0);
 }
+
+TEST(Play, TraitC)
+{
+  typedef play::TraitC<2,3> TraitC;
+  play::Expr<TraitC> e;
+  ASSERT_NO_THROW(TraitC::set_dims(2, 3));
+  ASSERT_EQ(6, e.product());
+  ASSERT_NE(8, e.product());
+  ASSERT_ANY_THROW(TraitC::set_dims(2, 2));
+}
+
+TEST(Play, TraitR)
+{
+  typedef typename play::TraitR TraitR;
+  play::Expr<TraitR> e;
+  ASSERT_EQ(0, e.product());
+
+  ASSERT_NO_THROW(TraitR::set_dims(2, 3));
+  ASSERT_EQ(6, e.product());
+  ASSERT_NE(8, e.product());
+
+  ASSERT_NO_THROW(TraitR::set_dims(2, 2));
+  ASSERT_EQ(4, e.product());
+}
