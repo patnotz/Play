692cb2c225d5dab36f1d5bd9fec5ee4a1ee0e763
Pat Notz <patnotz@gmail.com>
2011-08-17 16:34:22 -0600

    uncrustify and delint a la Google C++ code style

--- src/Play.hpp
+++ src/Play.hpp
@@ -2,26 +2,28 @@
 #ifndef SRC_PLAY_HPP_
 #define SRC_PLAY_HPP_
 
-#include <vector>
 #include <stdexcept>
+#include <vector>
 
 namespace play {
-
 struct ArrayR1 {
   ArrayR1()
     : m_ndim1(0) {}
-  ArrayR1(
-      const int ndim1)
+
+  explicit ArrayR1(const int ndim1)
     : m_ndim1(ndim1) {
     resize(ndim1);
   }
+
   void resize(const int ndim1) {
     m_ndim1 = ndim1;
     m_array.resize(ndim1);
   }
-  double & operator()(const int i) {
+
+  double& operator()(const int i) {
     return *(m_array.begin() + i);
   }
+
   int m_ndim1;
   std::vector<double> m_array;
 };
@@ -30,21 +32,29 @@ struct ArrayR2 {
   ArrayR2()
     : m_ndim1(0),
       m_ndim2(0) {}
+
   ArrayR2(
       const int ndim1,
       const int ndim2)
     : m_ndim1(ndim1),
       m_ndim2(ndim2) {
-    resize(ndim1,ndim2);
+    resize(ndim1, ndim2);
   }
-  void resize(const int ndim1, const int ndim2) {
+
+  void resize(
+      const int ndim1,
+      const int ndim2) {
     m_ndim1 = ndim1;
     m_ndim2 = ndim2;
     m_array.resize(ndim1*ndim2);
   }
-  double & operator()(const int i, const int j) {
+
+  double& operator()(
+      const int i,
+      const int j) {
     return *(m_array.begin() + i*m_ndim1 + j);
   }
+
   int m_ndim1;
   int m_ndim2;
   std::vector<double> m_array;
@@ -53,61 +63,75 @@ struct ArrayR2 {
 template <int NDIM1>
 struct ArrayC1 {
   ArrayC1() {}
-  ArrayC1(
-      const int ndim1) {
+
+  explicit ArrayC1(const int ndim1) {
     resize(ndim1);
   }
+
   void resize(const int ndim1) {
-    if (ndim1 != NDIM1) {
+    if (ndim1 != NDIM1)
       throw std::range_error("cannot change dims on compile time array");
-    }
   }
-  double & operator()(const int i) {
+
+  double& operator()(const int i) {
     return m_array[i];
   }
+
   double m_array[NDIM1];
 };
 
 template <int NDIM1, int NDIM2>
 struct ArrayC2 {
   ArrayC2() {}
+
   ArrayC2(
       const int ndim1,
       const int ndim2) {
-    resize(ndim1,ndim2);
+    resize(ndim1, ndim2);
   }
-  void resize(const int ndim1, const int ndim2) {
-    if (ndim1 != NDIM1 || ndim2 != NDIM2) {
+
+  void resize(
+      const int ndim1,
+      const int ndim2) {
+    if (ndim1 != NDIM1 || ndim2 != NDIM2)
       throw std::range_error("cannot change dims on compile time array");
-    }
   }
-  double & operator()(const int i, const int j) {
+
+  double& operator()(
+      const int i,
+      const int j) {
     return m_array[i][j];
   }
+
   double m_array[NDIM1][NDIM2];
 };
 
 // This "traits" class has the dimensions fixed at compile time
 template <int NDIM1, int NDIM2>
 struct TraitsC {
-  static void set_dims(const int dim1, const int dim2) {
-    if (dim1 != NDIM1 || dim2 != NDIM2) {
+  static void set_dims(
+      const int dim1,
+      const int dim2) {
+    if (dim1 != NDIM1 || dim2 != NDIM2)
       throw std::range_error("cannot change dims on compile time array");
-    }
   }
+
   static const int ndim1 = NDIM1;
   static const int ndim2 = NDIM2;
   typedef ArrayC1<NDIM1> ARRAY_1;
-  typedef ArrayC2<NDIM1,NDIM2> ARRAY_12;
+  typedef ArrayC2<NDIM1, NDIM2> ARRAY_12;
 };
 
 // This "traits" class allows you to change the dimensions
 // via static members at runtime
 struct TraitsR {
-  static void set_dims(const int dim1, const int dim2) {
+  static void set_dims(
+      const int dim1,
+      const int dim2) {
     ndim1 = dim1;
     ndim2 = dim2;
   }
+
   static int ndim1;
   static int ndim2;
   typedef ArrayR1 ARRAY_1;
@@ -121,8 +145,15 @@ int TraitsR::ndim2 = 0;
 // time will the optimizer unroll loops, etc.?
 struct ExprBase {
   virtual ~ExprBase() {}
-  virtual void set_dims(const int dim1, const int dim2) = 0;
-  virtual void eval(const int dim1, const int dim2) = 0;
+
+  virtual void set_dims(
+      const int dim1,
+      const int dim2) = 0;
+
+  virtual void eval(
+      const int dim1,
+      const int dim2) = 0;
+
   virtual double result() = 0;
 };
 
@@ -130,28 +161,35 @@ template <class T>
 struct Expr : ExprBase {
   typedef T Traits;
   virtual ~Expr() {}
-  virtual void set_dims(const int dim1, const int dim2) {
+
+  virtual void set_dims(
+      const int dim1,
+      const int dim2) {
     Traits::set_dims(dim1, dim2);
     values.resize(dim1);
-    vector.resize(dim1,dim2);
+    vector.resize(dim1, dim2);
   }
-  virtual void eval(const int dim1, const int dim2) {
+
+  virtual void eval(
+      const int dim1,
+      const int dim2) {
     set_dims(dim1, dim2);
-    for(int i=0; i < T::ndim1; ++i) {
+    for (int i = 0; i < T::ndim1; ++i) {
       values(i) = 0;
-      for(int j=0; j < T::ndim2; ++j) {
-        vector(i,j) = j+1;
-        values(i) += vector(i,j) * vector(i,j);
+      for (int j = 0; j < T::ndim2; ++j) {
+        vector(i, j) = j+1;
+        values(i) += vector(i, j) * vector(i, j);
       }
     }
   }
+
   virtual double result() {
     return values(0);
   }
+
   typename Traits::ARRAY_1 values;
   typename Traits::ARRAY_12 vector;
 };
-
 }  // namespace play
 
 #endif  // SRC_PLAY_HPP_
--- src/unit-tests.cpp
+++ src/unit-tests.cpp
@@ -1,51 +1,49 @@
-#include <gtest/gtest.h>
+// Copyright 2011 Patrick Notz
 #include <Play.hpp>
+#include <gtest/gtest.h>
 
-TEST(Play, CompileTimeTraits)
-{
+TEST(Play, CompileTimeTraits) {
   const int ndim1 = 2;
   const int ndim2 = 3;
-  typedef play::TraitsC<ndim1,ndim2> TraitsC;
+  typedef play::TraitsC<ndim1, ndim2> TraitsC;
   play::Expr<TraitsC> e;
-  ASSERT_NO_THROW(e.eval(ndim1,ndim2));
+  ASSERT_NO_THROW(e.eval(ndim1, ndim2));
   ASSERT_EQ(14, e.result());
   ASSERT_ANY_THROW(e.set_dims(ndim1+1, ndim2));
 }
 
-TEST(Play, RunTimeTraits)
-{
+TEST(Play, RunTimeTraits) {
   const int ndim1 = 2;
   const int ndim2 = 3;
   typedef typename play::TraitsR TraitsR;
   play::Expr<TraitsR> e;
 
-  ASSERT_NO_THROW(e.eval(ndim1,ndim2));
+  ASSERT_NO_THROW(e.eval(ndim1, ndim2));
   ASSERT_EQ(14, e.result());
   ASSERT_NO_THROW(e.eval(ndim1, ndim2+1));
   ASSERT_EQ(30, e.result());
 }
 
-TEST(Play, VirtualBase)
-{
+TEST(Play, VirtualBase) {
   const int ndim1 = 2;
   const int ndim2 = 3;
-  typedef play::TraitsC<ndim1,ndim2> TraitsC;
+  typedef play::TraitsC<ndim1, ndim2> TraitsC;
   typedef typename play::TraitsR TraitsR;
 
-  play::ExprBase * e = new play::Expr<TraitsR>();
-  ASSERT_NO_THROW(e->eval(ndim1,ndim2));
+  play::ExprBase* e = new play::Expr<TraitsR>();
+  ASSERT_NO_THROW(e->eval(ndim1, ndim2));
   ASSERT_EQ(14, e->result());
   ASSERT_NO_THROW(e->set_dims(ndim1+1, ndim2));
   delete e;
 
   e = new play::Expr<TraitsC>();
-  ASSERT_NO_THROW(e->eval(ndim1,ndim2));
+  ASSERT_NO_THROW(e->eval(ndim1, ndim2));
   ASSERT_EQ(14, e->result());
 
   // Hmmm, the code throws as expected but
   // GTest chokes on either of these
-  //ASSERT_ANY_THROW(e->set_dims(ndim1+1, ndim2));
-  //ASSERT_THROW(e->set_dims(ndim1+1, ndim2), std::range_error);
+  // ASSERT_ANY_THROW(e->set_dims(ndim1+1, ndim2));
+  // ASSERT_THROW(e->set_dims(ndim1+1, ndim2), std::range_error);
   // Hmmm, even manual exception checking fails even though
   // this works as expected in the other tests!
   delete e;
